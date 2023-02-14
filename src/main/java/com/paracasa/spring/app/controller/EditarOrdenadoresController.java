8c7e3ecc044deeb00727f6cf59cb890a25134291
ivresa9@gmail.com <Iv@nmisi0>
2023-02-14 19:16:25 +0100

    header modificado 2?

--- src/main/java/com/paracasa/spring/app/controller/EditarOrdenadoresController.java
+++ src/main/java/com/paracasa/spring/app/controller/EditarOrdenadoresController.java
@@ -3,7 +3,11 @@ package com.paracasa.spring.app.controller;
 
 import com.paracasa.spring.app.model.Ordenadores;
 import com.paracasa.spring.app.security.ordenadoresService.OrdenadoresService;
+import com.paracasa.spring.app.service.roleService.RoleService;
+import com.paracasa.spring.app.service.usuarioService.IUsuarioService;
+import com.paracasa.spring.app.utils.CheckSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -21,8 +26,16 @@ public class EditarOrdenadoresController {
     @Autowired
     private OrdenadoresService ordenadoresService;
 
+    @Autowired
+    private IUsuarioService userService;
+
+    @Autowired
+    private RoleService roleService;
+
     @GetMapping("/ordenadores/editar/{id}")
-    public String editar(@PathVariable("id") long id, Model model) {
+    public String editar(@PathVariable("id") long id, Model model, Authentication auth, HttpSession session) {
+        this.checkSession(auth, session,
+                userService, roleService);
         Optional<Ordenadores> ordenador = ordenadoresService.findById(id);
         model.addAttribute("ordenador", ordenador);
         model.addAttribute("metaTitle", "Editar ordenador");
@@ -36,7 +49,17 @@ public class EditarOrdenadoresController {
             return "editar_ordenador";
         }
         ordenador.setId(id);
-        ordenadoresService.update(ordenador);
+        ordenadoresService.delete(id);
         return "redirect:/ordenadores";
+    }
+
+
+
+    private void checkSession(Authentication auth, HttpSession session,
+                              IUsuarioService userService,
+                              RoleService roleService){
+        CheckSession currentSession = new CheckSession(auth, session,
+                userService, roleService);
+        currentSession.validate();
     }
 }
--- src/main/java/com/paracasa/spring/app/controller/OrdenadoresController.java
+++ src/main/java/com/paracasa/spring/app/controller/OrdenadoresController.java
@@ -1,21 +1,34 @@
 package com.paracasa.spring.app.controller;
 
 import com.paracasa.spring.app.security.ordenadoresService.OrdenadoresService;
+import com.paracasa.spring.app.service.roleService.RoleService;
+import com.paracasa.spring.app.service.usuarioService.IUsuarioService;
+import com.paracasa.spring.app.utils.CheckSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class OrdenadoresController {
 
     @Autowired
     private OrdenadoresService ordenadoresService;
+    @Autowired
+    private IUsuarioService userService;
 
+    @Autowired
+    private RoleService roleService;
     @RequestMapping(value="/ordenadores")
-    public String index(Model model) {
+
+    public String index(Model model, Authentication auth, HttpSession session) {
+        this.checkSession(auth, session,
+                userService, roleService);
         model.addAttribute("metaTitle", "Ordenadores");
         model.addAttribute("ordenadores", ordenadoresService.findAll());
 
@@ -28,5 +41,13 @@ public class OrdenadoresController {
         return "redirect:/ordenadores";
     }
 
+
+    private void checkSession(Authentication auth, HttpSession session,
+                              IUsuarioService userService,
+                              RoleService roleService){
+        CheckSession currentSession = new CheckSession(auth, session,
+                userService, roleService);
+        currentSession.validate();
+    }
 
 }
